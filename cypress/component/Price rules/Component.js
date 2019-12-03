80afca20321a7869b981c02859a348fca4580883
temelkovskabyanka <temelkovska.byanka@gmail.com>
2019-12-03 22:13:45 +0000

    new component

--- cypress/component/Price rules/Component.js
+++ cypress/component/Price rules/Component.js
@@ -1,17 +1,26 @@
-export default class Component{
-    get loginButton(){
-    return cy.get(".btn.btn-primary.btn-block");
+export default class Component {
+    get loginButton() {
+        return cy.get(".btn.btn-primary.btn-block");
     };
-   get  searchPriceRulesField(){
+
+    get priceRulesIconButton() {
+        return cy.get("#mainMenu");
+    };
+
+    get searchPriceRulesField() {
         return cy.get("#search");
     }
-    get priceRulesText(){
-       return cy.get(".col-md-12.text-center.text-warning");
+
+    get priceRulesText() {
+        return cy.get(".col-md-12.text-center.text-warning");
     }
-    get exitSearchButton(){
-       return cy.get(".ion.ion-close-circled");
+
+    get exitSearchButton() {
+        return cy.get(".ion.ion-close-circled");
     }
-    get totalPriceRulesText(){
-       return cy.get("#manage_total_items");
+
+    get totalPriceRulesText() {
+        return cy.get("#manage_total_items");
     }
+
 }
